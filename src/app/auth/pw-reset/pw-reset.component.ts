0be32355158bd4b8b517f6abc815264b23176558
Sebastian Brosda <brosda90@web.de>
2024-02-07 21:58:14 +0100

    first commit

---
+++ src/app/auth/pw-reset/pw-reset.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pw-reset',
+  standalone: true,
+  imports: [],
+  templateUrl: './pw-reset.component.html',
+  styleUrl: './pw-reset.component.scss'
+})
+export class PwResetComponent {
+
+}
---
+++ src/app/main-channel/threads/threads.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-threads',
+  standalone: true,
+  imports: [],
+  templateUrl: './threads.component.html',
+  styleUrl: './threads.component.scss'
+})
+export class ThreadsComponent {
+
+}
