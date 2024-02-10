e322859fcf7307c08a7c4ecb9aa15cdb102f5f1b
tobiastermer <tobiastermer@gmx.de>
2024-02-10 12:46:46 +0100

    menu channels first commit

--- src/app/main-content/menue/menue-channels/menue-channels.component.ts
+++ src/app/main-content/menue/menue-channels/menue-channels.component.ts
@@ -1,12 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-menue-channels',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, CommonModule],
   templateUrl: './menue-channels.component.html',
   styleUrl: './menue-channels.component.scss'
 })
 export class MenueChannelsComponent {
 
+  channels: string[] = ['Entwicklerteam', 'Office-Team', 'Plauderecke', 'Coffee-Corner'];
 }
