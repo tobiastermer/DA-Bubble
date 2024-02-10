a0e148a0de4f43bc7f3bb3f2b232eb6a1cf19274
tobiastermer <tobiastermer@gmx.de>
2024-02-10 11:54:28 +0100

    menue header

--- src/app/main-content/menue/menue-header/menue-header.component.ts
+++ src/app/main-content/menue/menue-header/menue-header.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-menue-header',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './menue-header.component.html',
   styleUrl: './menue-header.component.scss'
 })
