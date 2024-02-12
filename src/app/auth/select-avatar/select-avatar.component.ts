8fc0c54e1b7408c1aa9a8e4667aa291c0c2a23bd
Sebastian Brosda <brosda90@web.de>
2024-02-12 16:12:42 +0100

    Enhanced Sign up & Avatar component with HTML structure and SCSS styling

--- src/app/auth/select-avatar/select-avatar.component.ts
+++ src/app/auth/select-avatar/select-avatar.component.ts
@@ -1,12 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-select-avatar',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule,MatIconModule,CommonModule],
   templateUrl: './select-avatar.component.html',
   styleUrl: './select-avatar.component.scss'
 })
 export class SelectAvatarComponent {
-
+  avatars = ['1.svg', '2.svg', '3.svg', '4.svg', '5.svg'];
 }
--- src/app/auth/sign-up/sign-up.component.ts
+++ src/app/auth/sign-up/sign-up.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatFormFieldModule,MatInputModule,MatIconModule,MatCheckboxModule],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.scss'
 })
