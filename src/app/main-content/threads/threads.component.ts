3f1afc9ae88eca240964dfa217b394c5a48af8c5
Sebastian Brosda <brosda90@web.de>
2024-02-12 11:56:25 +0100

    Enhanced Threads component with HTML structure and SCSS styling

--- src/app/main-content/threads/threads.component.ts
+++ src/app/main-content/threads/threads.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatCardModule} from '@angular/material/card';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-threads',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule,MatDividerModule,CommonModule,MatCardModule],
   templateUrl: './threads.component.html',
   styleUrl: './threads.component.scss'
 })
 export class ThreadsComponent {
 
+  
 }
