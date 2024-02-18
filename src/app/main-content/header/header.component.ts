7e497fc3f926c76f4a70feba4c4db19c8b766d88
Ricki W. <ricki.weigel@gmx.de>
2024-02-18 01:19:33 +0100

    add header

--- src/app/main-content/header/header.component.ts
+++ src/app/main-content/header/header.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [MatInputModule,MatIconModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
