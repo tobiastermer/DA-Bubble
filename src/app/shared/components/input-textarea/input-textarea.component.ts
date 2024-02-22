b1b0050d5e43361e5b7b7d64b7118abe5574bf7d
Navid Pazoki <navid.pazoki@gmx.de>
2024-02-22 10:29:03 +0100

    Included sending functions on the input-textarea component to send messages in the thread area

--- src/app/shared/components/input-textarea/input-textarea.component.ts
+++ src/app/shared/components/input-textarea/input-textarea.component.ts
@@ -4,6 +4,7 @@ import { ChannelMessage } from '../../models/channel-message.class';
 import { DataService } from '../../services/data.service';
 import { ChannelMessagesService } from '../../firebase-services/channel-message.service';
 import { Channel } from '../../models/channel.class';
+import { Reply } from '../../models/reply.class';
 
 
 @Component({
@@ -15,8 +16,9 @@ import { Channel } from '../../models/channel.class';
 })
 export class InputTextareaComponent {
 
-  @Input() channel!: Channel;
+  @Input() channel!: Channel | undefined;
   @Input() msg!: ChannelMessage;
+  @Input() channelMsg: Boolean = false;
 
   @ViewChild('messageText') messageText!: ElementRef;
 
@@ -43,23 +45,53 @@ export class InputTextareaComponent {
     }
   }
 
-  async addNewMsg(text: string) {
-    let newMsg: ChannelMessage | undefined = this.fillMessage(text)
-    if (!newMsg) return
-    else {
-      let id = await this.messageFBS.addChannelMessage(newMsg);
-    }
+
+  addNewMsg(text: string) {
+    if (this.channelMsg) this.addNewChannelMsg(text)
+    else this.addNewReplyMsg(text)
   }
 
-  fillMessage(text: string) {
+
+  async addNewChannelMsg(text: string) {
+    let newMsg: ChannelMessage | undefined = this.fillChannelMsg(text);
+    let id: any;
+    if (!newMsg) return
+    id = await this.messageFBS.addChannelMessage(newMsg);
+    if (!id) return
+    newMsg.id = id;
+    await this.messageFBS.updateChannelMessage(newMsg);
+  }
+
+
+  fillChannelMsg(text: string) {
     if (!this.data.currentUser.id) return
-    if (!this.channel.id || this.channel.id === '') return
+    if (!this.channel|| this.channel.id === '') return
     let msg = new ChannelMessage();
     msg.date = new Date().getTime();
     msg.channelID = this.channel.id;
     msg.fromUserID = this.data.currentUser.id;
     msg.message = text;
     return msg
+  }
+
+
+  async addNewReplyMsg(text: string) {
+    let newReply: Reply | undefined = this.fillReplyMsg(text)
+    if (!newReply || !this.msg) return
+    this.msg.replies.push(newReply);
+    await this.messageFBS.updateChannelMessage(this.msg);
+  }
+
+
+  fillReplyMsg(text: string) {
+    if (!this.data.currentUser.id) return
+    if (!this.channel || this.channel.id === '') return
+    let reply = new Reply();
+    reply.date = new Date().getTime();
+    reply.channelID = this.channel.id;
+    reply.userID = this.data.currentUser.id;
+    reply.message = text;
+    return reply
   }
 
 
