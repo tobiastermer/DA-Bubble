04464f807d498abec3f803a5bbbfb2b3b187d165
RickiWeigel <ricki.weigel@gmx.de>
2024-02-24 09:26:05 +0100

    add show profil user-dialog

--- src/app/main-content/header/header.component.ts
+++ src/app/main-content/header/header.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchBarComponent } from './search-bar/search-bar.component';
-import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatDialog, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MenuDialogComponent } from './menu-dialog/menu-dialog.component';
 import { DataService } from '../../shared/services/data.service';
 
@@ -17,10 +17,9 @@ export class HeaderComponent {
 
   dropDown: boolean = false;
 
-  constructor(public dialog: MatDialog, public data: DataService) {}
+  constructor(public dialog: MatDialog, public data: DataService,) {}
 
   openDialog() {
-    
     this.dialog.open(MenuDialogComponent,{
       panelClass: ['card-right-corner'],
       position: {
@@ -28,8 +27,9 @@ export class HeaderComponent {
         right: '20px'
       }
     });
-
+    console.log(this.data.currentUser)
   }
+
 
   toggleDropDown() {
     if (this.dropDown) {
--- src/app/main-content/header/menu-dialog/menu-dialog.component.ts
+++ src/app/main-content/header/menu-dialog/menu-dialog.component.ts
@@ -1,8 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatDialogRef, MatDialogContent } from '@angular/material/dialog';
+import {
+  MatDialogRef,
+  MatDialogContent,
+  MatDialog,
+} from '@angular/material/dialog';
 import { DataService } from '../../../shared/services/data.service';
-
+import { DialogShowUserComponent } from '../../../shared/components/dialogs/dialog-show-user/dialog-show-user.component';
+import { User } from '../../../shared/models/user.class';
 
 @Component({
   selector: 'app-menu-dialog',
@@ -14,8 +19,25 @@ import { DataService } from '../../../shared/services/data.service';
 export class MenuDialogComponent {
   @Input() dropDown: boolean = false;
 
-  constructor(public dialogRef: MatDialogRef<MenuDialogComponent>, public data: DataService) {
+  constructor(
+    public dialogRef: MatDialogRef<MenuDialogComponent>,
+    public data: DataService,
+    public dialog: MatDialog
+  ) {}
 
-    console.log(data.currentUser)
+  closeDialog() {
+    this.dialogRef.close();
+  }
+
+  openDialog(user: User, currentUserID: String) {
+    this.dialog.open(DialogShowUserComponent, {
+      panelClass: ['card-right-corner'],
+      position: {
+        top: '90px',
+        right: '20px',
+      },
+      data: { user, currentUserID },
+    });
+    this.closeDialog();
   }
 }
