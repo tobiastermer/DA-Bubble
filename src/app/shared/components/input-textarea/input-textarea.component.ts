e006d3368561792380134217accbf50766797fff
Navid Pazoki <navid.pazoki@gmx.de>
2024-03-01 14:25:40 +0100

    Extracted file functions from input-textarea component to file.service and added functions for deleting and checking uploaded files in input-textarea component

--- src/app/shared/components/input-textarea/input-textarea.component.ts
+++ src/app/shared/components/input-textarea/input-textarea.component.ts
@@ -15,6 +15,7 @@ import { DialogShowUserComponent } from '../dialogs/dialog-show-user/dialog-show
 import { AuthService } from '../../firebase-services/auth.service';
 import { DialogInfoComponent } from '../dialogs/dialog-info/dialog-info.component';
 import { AddEmojiService } from '../../services/add-emoji.service';
+import { FileService } from '../../services/file.service';
 
 
 @Component({
@@ -53,13 +54,28 @@ export class InputTextareaComponent {
     private messageFBS: ChannelMessagesService,
     private PositionService: PositionService,
     public dialog: MatDialog,
-    private authService: AuthService,
-    public addEmoji: AddEmojiService
-    ) { }
+    public addEmoji: AddEmojiService,
+    private fileService: FileService
+  ) { }
 
 
-  updateButtonState(textValue: string): void {
-    this.isButtonDisabled = !textValue.trim();
+  updateButtonState() {
+    if (this.messageText && this.messageText.nativeElement.innerText.trim()) return this.isButtonDisabled = false
+    if (this.tempFile) return this.isButtonDisabled = false
+    return this.isButtonDisabled = true
+  }
+
+
+  onDelete(event: Event) {
+    if (!(event instanceof KeyboardEvent)) return
+    if (event.key !== 'Backspace') return
+    if (!this.messageText.nativeElement.querySelector('#uploadFile')) return
+    setTimeout(() => {
+      if (!this.messageText.nativeElement.querySelector('#uploadFile')) {
+        this.tempFile = undefined;
+        this.updateButtonState();
+      }
+    }, 5);
   }
 
 
@@ -99,7 +115,7 @@ export class InputTextareaComponent {
     msg.channelID = this.channel.id;
     msg.fromUserID = this.data.currentUser.id;
     msg.message = text.replace(/^\n+/, '');
-    msg.attachmentID = await this.uploadFile();
+    msg.attachmentID = await this.fileService.uploadFile(this.tempFile);
     return msg
   }
 
@@ -120,7 +136,7 @@ export class InputTextareaComponent {
     reply.channelID = this.channel.id;
     reply.userID = this.data.currentUser.id;
     reply.message = text;
-    reply.attachmentID = await this.uploadFile();
+    reply.attachmentID = await this.fileService.uploadFile(this.tempFile);
     return reply
   }
 
@@ -145,6 +161,7 @@ export class InputTextareaComponent {
     this.addEmoji.setCurserToEndPos(this.range);
   }
 
+
   appendChildForAllUsers(users: User[]) {
     users.forEach((user, index) => {
       let span = document.createElement('span');
@@ -158,6 +175,7 @@ export class InputTextareaComponent {
     });
   }
 
+
   openShowUserDialog(user: User) {
     this.dialog.open(DialogShowUserComponent, {
       panelClass: ['card-round-corners'],
@@ -165,7 +183,6 @@ export class InputTextareaComponent {
     });
   }
 
-  // Emoji part
 
   openDialogEmoji(): void {
     const selection = window.getSelection();
@@ -177,80 +194,28 @@ export class InputTextareaComponent {
       data: {},
     });
     dialogRef.afterClosed().subscribe(result => {
-      if (result) this.addEmoji.addEmoji(this.range, result, this.messageText )
+      if (!result) return
+      this.addEmoji.addEmoji(this.range, result, this.messageText);
+      this.updateButtonState();
     });
   }
 
 
+  selectFile(event: any) {
+    if (this.tempFile) this.changeFile();
+    this.fileService.element = this.messageText;
+    this.tempFile = this.fileService.onFileSelected(event);
+    this.updateButtonState();
+  }
 
-  
-  onFileSelected(event: any) {
-    const file = event.target.files[0];
-    const supportedFileTypes = ['image/jpeg', 'image/png', 'application/pdf']
-    if (!file) return;
-    if ((file.size / 1024 / 1024) > 1) {
-      return this.openDialogInfo('Ihre Datei ist zu groß. Maximale Größe beträgt 1.5MB.');
+
+  changeFile() {
+    let uploadFileElement = this.messageText.nativeElement.querySelector('#uploadFile');
+    if (uploadFileElement) {
+      let parentElement = uploadFileElement.parentElement;
+      parentElement.removeChild(uploadFileElement);
     }
-    if (!supportedFileTypes.includes(file.type)) {
-      return this.openDialogInfo('Nur .jpg / .png oder .pdf Dateiformate werden unterstützt.');
-    }
-    this.tempFile = file;
-    this.addFileToMessageText(file);
-  }
-
-
-  openDialogInfo(info: String): void {
-    this.dialog.open(DialogInfoComponent, {
-      panelClass: ['card-round-corners'],
-      data: { info },
-    });
-  }
-
-
-  addFileToMessageText(file: File) {
-    if (!this.messageText) return;
-    this.appendChildForFile(file);
-    if (!this.range) return
-    this.addEmoji.setCurserToEndPos(this.range);
-  }
-
-
-  appendChildForFile(file: File) {
-    let div = document.createElement('div');
-    div.contentEditable = 'false';
-    div.classList.add('file-overview');
-    div.appendChild(this.getImgByFileType(file));
-    div.appendChild(this.getSpanByFileName(file));
-    this.messageText.nativeElement.insertBefore(div, this.messageText.nativeElement.firstChild);
-    this.messageText.nativeElement.appendChild(document.createElement('br'));
-  }
-
-
-  getImgByFileType(file: File): Element {
-    let img = document.createElement('img');
-    img.src = 'assets/img/icons/upload_file.png';
-    img.alt = file.type;
-    return img
-  }
-
-
-  getSpanByFileName(file: File): Element {
-    let span = document.createElement('span');
-    span.innerText = file.name;
-    return span
-  }
-
-
-  async uploadFile() {
-    if (!this.tempFile) return ''
-    let retUrl = '';
-    await this.authService.uploadMsgData(this.tempFile).then((url) => {
-      retUrl = url;
-    }).catch((error) => {
-      console.error("Fehler beim Hochladen des Bildes: ", error);
-      this.openDialogInfo("Fehler beim Hochladen des Bildes.");
-    });
-    return retUrl
+    this.tempFile = undefined;
   }
 
 }
---
+++ src/app/shared/services/file.service.ts
@@ -0,0 +1,90 @@
+import { ElementRef, Injectable } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogInfoComponent } from '../components/dialogs/dialog-info/dialog-info.component';
+import { AuthService } from '../firebase-services/auth.service';
+import { File } from 'buffer';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FileService {
+
+  element!: ElementRef;
+
+  constructor(
+    public dialog: MatDialog,
+    private authService: AuthService,
+  ) { }
+
+
+  onFileSelected(event: any) {
+    const file = event.target.files[0];
+    const supportedFileTypes = ['image/jpeg', 'image/png', 'application/pdf']
+    if (!file) return;
+    if ((file.size / 1024 / 1024) > 1) {
+      return this.openDialogInfo('Ihre Datei ist zu groß. Maximale Größe beträgt 1.5MB.');
+    }
+    if (!supportedFileTypes.includes(file.type)) {
+      return this.openDialogInfo('Nur .jpg / .png oder .pdf Dateiformate werden unterstützt.');
+    }
+    // this.tempFile = file;
+    this.addFileToMessageText(file);
+    return file
+  }
+
+
+  openDialogInfo(info: String): void {
+    this.dialog.open(DialogInfoComponent, {
+      panelClass: ['card-round-corners'],
+      data: { info },
+    });
+  }
+
+
+  addFileToMessageText(file: File) {
+    if (!this.element) return;
+    this.appendChildForFile(file);
+    // if (!this.range) return
+    // this.addEmoji.setCurserToEndPos(this.range);
+  }
+
+
+  appendChildForFile(file: any) {
+    let div = document.createElement('div');
+    div.contentEditable = 'false';
+    div.classList.add('file-overview');
+    div.appendChild(this.getImgByFileType(file));
+    div.appendChild(this.getSpanByFileName(file));
+    div.id = 'uploadFile';
+    this.element.nativeElement.insertBefore(div, this.element.nativeElement.firstChild);
+    this.element.nativeElement.appendChild(document.createElement('br'));
+  }
+
+
+  getImgByFileType(file: File): Element {
+    let img = document.createElement('img');
+    img.src = 'assets/img/icons/upload_file.png';
+    img.alt = file.type;
+    return img
+  }
+
+
+  getSpanByFileName(file: globalThis.File): Element {
+    let span = document.createElement('span');
+    span.innerText = file.name;
+    return span
+  }
+
+
+  async uploadFile(file: globalThis.File) {
+    if (!this.element) return ''
+    let retUrl = '';
+    await this.authService.uploadMsgData(file).then((url) => {
+      retUrl = url;
+    }).catch((error) => {
+      console.error("Fehler beim Hochladen des Bildes: ", error);
+      this.openDialogInfo("Fehler beim Hochladen des Bildes.");
+    });
+    return retUrl
+  }
+}
